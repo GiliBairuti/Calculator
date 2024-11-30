761e7cae1e3bbff0399643370eae1447d9fcfd26
unknown <gilibairuti@gmail.com>
2024-11-30 18:12:09 +0200

    I updated classmethod to staticmethod

--- math_interface.py
+++ math_interface.py
@@ -5,7 +5,7 @@ class ArithmeticFuncs(ABC):
     """
     the abstract class of the arithmetic methods there are in the calculator
     """
-    @classmethod
+    @staticmethod
     @abstractmethod
     def resolve(cls, arg1, arg2=0):
         ...
